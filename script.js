a0246b50cec24b6aaf795a01d19364377cfeb71f
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-08-02 17:00:48 +0200

    comments

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('JS OK');
+
+// Prelevare file di ineteresse dal DOM
+// usare FOR per stampare in console numeri da 1 a 100
+// Controllare tramite IF se i numeri siano multipli di 3 / di 5 / di 3 e 5
+// Dare classi diverse a questi elementi per poi modificarli con CSS
+// Chiedere all'utente quanti numeri si vogliono stampare 
+// Stampare nel DOM
+
+//FASE PREPARATORIA
+//Prendo il file dal DOM
+const target = document.getElementById('target');
+
+// ELABORAZIONE
