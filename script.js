a80250fa90cb7b51a68f3951e65c25f5897fbe9c
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-08-02 17:20:32 +0200

    added for 1-100

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ console.log('JS OK');
 // usare FOR per stampare in console numeri da 1 a 100
 // Controllare tramite IF se i numeri siano multipli di 3 / di 5 / di 3 e 5
 // Dare classi diverse a questi elementi per poi modificarli con CSS
-// Chiedere all'utente quanti numeri si vogliono stampare 
 // Stampare nel DOM
 
 //FASE PREPARATORIA
@@ -12,3 +11,10 @@ console.log('JS OK');
 const target = document.getElementById('target');
 
 // ELABORAZIONE
+let numbers = '';
+
+for (let i = 1; i <= 100; i++) {
+    console.log(`${i}`);
+    //numbers += `<div class="num">${i}</div`;
+}
+
